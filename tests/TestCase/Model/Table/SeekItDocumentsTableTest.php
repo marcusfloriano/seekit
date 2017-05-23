2d706b7b60eed15ebc62d4eae7136e113c79d198
Marcus Floriano <marcus.floriano@gmail.com>
2017-05-23 09:34:52 -0300

    create the test for new document and delete the document

--- tests/TestCase/Model/Table/SeekItDocumentsTableTest.php
+++ tests/TestCase/Model/Table/SeekItDocumentsTableTest.php
@@ -58,7 +58,6 @@ class SeekItDocumentsTableTest extends TestCase
      */
     public function testCreateNewDocument()
     {
-
         $article = $this->getMockBuilder('Article')
             ->setMethods(['getId','getTitle','getSubTitle','getBody'])
             ->getMock();
@@ -77,17 +76,44 @@ class SeekItDocumentsTableTest extends TestCase
 
         if($this->SeekItDocuments->save($seek_document)){
             $this->assertGreaterThan(1, $seek_document->id);
-            debug($seek_document);
+            $article_unserialize = unserialize($seek_document->serialize);
+            $this->assertEquals($article_unserialize->getId(),"ABCDEFG");
+            $this->assertEquals($article_unserialize->getTitle(),"New document for add");
         }
     }
 
-    /**
-     * Test validationDefault method
-     *
-     * @return void
-     */
-    public function testValidationDefault()
+    public function testDeleteDocument()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $article = $this->getMockBuilder('Article')
+            ->setMethods(['getId','getTitle','getSubTitle','getBody'])
+            ->getMock();
+        $article->method('getId')->willReturn('ABCDEFG');
+        $article->method('getTitle')->willReturn('New document for add');
+        $article->method('getSubTitle')->willReturn("The subtitle of new document");
+        $article->method('getBody')->willReturn("The body of document, this contains all text that represent this document.");
+        
+        $seek_document = $this->SeekItDocuments->newEntity();
+        $seek_document->title = $article->getTitle();
+        $seek_document->subtitle = $article->getSubTitle();
+        $seek_document->body = $article->getBody();
+        $seek_document->refid = $article->getId();
+        $seek_document->reftype = get_class($article);
+        $seek_document->serialize = serialize($article);
+
+        if($this->SeekItDocuments->save($seek_document)){
+            $this->assertGreaterThan(1, $seek_document->id);
+            $article_unserialize = unserialize($seek_document->serialize);
+            $this->assertEquals($article_unserialize->getId(),"ABCDEFG");
+            $this->assertEquals($article_unserialize->getTitle(),"New document for add");
+        }
+
+        $seek_document_for_delete = $this->SeekItDocuments->find()->where(['refid' => 'ABCDEFG'])->first();
+        $this->assertNotNull($seek_document_for_delete);
+        $this->assertEquals($seek_document_for_delete->refid,"ABCDEFG");
+
+        $this->SeekItDocuments->delete($seek_document_for_delete);
+        $this->assertEquals($this->SeekItDocuments->find()->count(), 1);
     }
+
+
 }
